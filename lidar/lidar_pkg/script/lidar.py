be4044d71ee953c6d4c55417d0671fc55164212b
py-al3xis <al3xism0rice@gmail.com>
2026-05-13 01:31:23 +0200

    added the blinking led

--- lidar/lidar_pkg/script/lidar.py
+++ lidar/lidar_pkg/script/lidar.py
@@ -4,7 +4,7 @@ from rclpy.node import Node
 from sensor_msgs.msg import LaserScan
 from lidar_pkg.msg import ObstacleStatus
 import math
-import RPi.GPIO as GPIO
+from gpiozero import LED
 
 
 
@@ -12,23 +12,46 @@ class LidarNode(Node):
     ranges = []
     min_dist = 0.5      #in meters
     fov = math.pi/2
-    LED_GPIO_ = 20
+    LED_GPIO_ = LED(17)
+ 
+
+    on_duration = 0.5   
+    off_duration = 5.0  
+    is_led_on = False
+    
+
+
 
     def __init__(self):
         super().__init__("lidar")
         self.lidar_subscriber = self.create_subscription(LaserScan, "scan", self.callback_lidar_subscriber, 10)
         self.operation_timer = self.create_timer(1.0, self.main_loop)
         self.velocity_publisher = self.create_publisher(ObstacleStatus, "max_speed", 10)
-        GPIO.setmode(GPIO.BCM)
-        GPIO.setup(self.LED_GPIO_, GPIO.OUT)
-        GPIO.cleanup()
+        self.timer = self.create_timer(0.05, self.blink_led)
+        self.last_toggle_time = self.get_clock().now()
+
+    def blink_led(self):
+
+        now = self.get_clock().now()
+        elapsed = (now - self.last_toggle_time).nanoseconds / 1e9
+
+        if self.is_led_on:
+            if elapsed >= self.on_duration:
+                self.LED_GPIO_.off()
+                self.is_led_on = False
+                self.last_toggle_time = now
+        else:
+            if elapsed >= self.off_duration:
+                self.LED_GPIO_.on()
+                self.is_led_on = True
+                self.last_toggle_time = now
 
     def callback_lidar_subscriber(self, msg:LaserScan):
         self.ranges = list(msg.ranges)
 
     def main_loop(self):
         max_range = 3.0   # The length of the playing board
-
+        
         if self.ranges == []:
             pass     
         else: 
